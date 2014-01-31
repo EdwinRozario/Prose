52b9b45eece75a874df496fbc637165bcdb18330
EdwinRozario <rozarioed@gmail.com>
2014-02-01 02:48:25 +1300

    First Commit

---
+++ lib/porse.rb
@@ -0,0 +1,34 @@
+require 'yaml'
+
+class String
+
+  def porse?
+  	find_languages_in(self) 
+  end
+
+  private
+
+
+  def unicode_ranges
+  	@ranges ||= YAML::load( File.open( "#{File.expand_path File.dirname(__FILE__)}/porse/porse.yaml" ) )
+  end
+
+  def language_of letter
+  	result = []
+  	int_ordinal = letter.ord
+  	unicode_ranges.keys.each do |key|
+  	  min, max = key.split("-")
+  	  result << unicode_ranges[key] if (min.to_i(16) < int_ordinal) and (max.to_i(16) > int_ordinal)
+  	end
+  	return result
+  end
+
+  def find_languages_in word
+  	result = []
+  	word.split('').each do |letter|
+  	  result += language_of(letter) if not letter == " "
+  	end
+  	return result.uniq
+  end
+
+end
---
+++ test/test.rb
@@ -0,0 +1,5 @@
+p "لعربية".porse?
+p "ಕನ್ನಡ".porse?
+p "what".porse?
+p "עִבְרִית".porse?
+p "fעִةಕt".porse?
